d762cbf4106514113e1daa74cb78be2ead618ba5
bautistaamarillo <amarillobautista5@gmail.com>
2022-04-08 21:03:37 -0300

    trabajo hecho en clase viernes 8_04

---
+++ trabajo2/js/scripts/funciones.js
@@ -0,0 +1,22 @@
+
+
+function saludar(){
+    console.log ('Hola');    
+    }
+    
+function despedir(){
+    console.log ('Chau');    
+    }
+function alerta(){
+    alert("Alerta");
+}
+function cambiarcolor(){
+ document.getElementById("a1").style.color="red";
+}
+function cambiarfondo(){
+    document.getElementById("a1").style.backgroundColor="grey";
+}
+function volver(){
+    document.getElementById("a1").style.backgroundColor="blue";
+    document.getElementById("a1").style.color="white";
+}
