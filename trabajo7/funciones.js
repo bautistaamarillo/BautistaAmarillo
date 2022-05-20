94737af8820839db0625e2e5e1e55d7a70e9c16a
bautistaamarillo <amarillobautista5@gmail.com>
2022-05-19 23:27:08 -0300

    Trabajo 7 v2

--- trabajo7/funciones.js
+++ trabajo7/funciones.js
@@ -1,5 +1,3 @@
-
-
 function botonfinal() {
     fetch("https://randomuser.me/api")
     .then (res => res.json())
@@ -14,33 +12,32 @@ function botonfinal() {
         let email = data.results[0].email;
         let foto = data.results[0].picture.large;
         
-
         
-    
-         document.getElementById("nombre_id").innerHTML = "Nombre : "+nombre;
-         document.getElementById("apellido_id").innerHTML = "Apellido : "+apellido;
-         document.getElementById("sexo_id").innerHTML = "Sexo : "+sexo;
-         document.getElementById("edad_id").innerHTML = "Edad : "+edad;
-         document.getElementById("email_id").innerHTML = "Email : "+email;
-         document.getElementById("foto_id").innerHTML = "Foto: " + `<img src= "${foto}">`
+        //  document.getElementById("nombre_id").innerHTML = "Nombre : "+nombre;
+        //  document.getElementById("apellido_id").innerHTML = "Apellido : "+apellido;
+        //  document.getElementById("sexo_id").innerHTML = "Sexo : "+sexo;
+        //  document.getElementById("edad_id").innerHTML = "Edad : "+edad;
+        //  document.getElementById("email_id").innerHTML = "Email : "+email;
+        //  document.getElementById("foto_id").innerHTML = "Foto: " + `<img src= "${foto}">`
 
-  
-
-       
+        let html = `Nombre: ${nombre}  <br>
+        Apellido: ${apellido} <br>
+        Sexo: ${sexo} <br>
+        Edad: ${edad} <br>
+        Email: ${email} <br>
+        Foto: <img src= "${foto}"> `
+        document.getElementById("datos_id").innerHTML = html
     })
 
-//extra boton reset
 
-    }
-
+}
+      //**** Boton extra de prueba *****
     function botonreset() {  
     location.reload()
-    
     }
 
 
 
     //consigna :
     // En la pagina web que tenemos nosotros, tiene que estar dibujado un boton que diga solicitar datos
-    // y que me aparezca en la pantalla el nombre:, el apellido, el sexo, edad, y el email.
-
+    // y que me aparezca en la pantalla el nombre:, el apellido, el sexo, edad, email, y una foto de la persona
